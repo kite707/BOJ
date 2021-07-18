f365600b83928816e56eadaf08fd2ad6b1fff799
kite707 <70741257+kite707@users.noreply.github.com>
2021-07-18 16:46:57 +0900

    Update 10816.cpp

--- 10816.cpp
+++ 10816.cpp
@@ -1,23 +1,48 @@
-//#include <iostream>
-//using namespace std;
-//
-//int arr[10001] = { 0 };
-//
-//int main() {
-//	ios::sync_with_stdio(false);
-//	cin.tie(NULL);
-//	cout.tie(NULL);
-//
-//	int t;
-//	cin >> t;
-//	for (int i = 0; i < t; i++) {
-//		int k;
-//		cin >> k;
-//		arr[k]++;
-//	}
-//	for (int i = 0; i < 10001; i++) {
-//		for (int k = 0; k < arr[i]; k++) {
-//			cout << i << "\n";
-//		}
-//	}
-//}
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int vecplus[10000001] = { 0 };
+int vecminus[10000001] = { 0 };
+vector<int> que;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int N;
+	int a;
+	int zero = 0;
+	cin >> N;
+	while (N--) {
+		cin >> a;
+		if (a > 0) {
+			vecplus[a]++;
+		}
+		if (a < 0) {
+			a = a * -1;
+			vecminus[a]++;
+		}
+		if(a==0) {
+			zero++;
+		}
+	}
+	int M;
+	cin >> M;
+	while (M--) {
+		cin >> a;
+		que.push_back(a);
+	}
+	for (int i = 0; i < que.size(); i++) {
+		if (que[i] > 0) {
+			cout << vecplus[que[i]] << " ";
+		}
+		else if (que[i] < 0) {
+			int jj = que[i] * -1;
+			cout << vecminus[jj] << " ";
+		}
+		else {
+			cout << zero << " ";
+		}
+	}
+}
